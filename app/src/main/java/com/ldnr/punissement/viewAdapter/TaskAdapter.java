cac2d6ce83d2c84d52a9255856afb167d9ac4d4e
JB <jb.vdw905@gmail.com>
2018-05-24 10:40:12 +0200

    commit de tasklist & co

---
+++ app/src/main/java/com/ldnr/punissement/viewAdapter/TaskAdapter.java
@@ -0,0 +1,45 @@
+package com.ldnr.punissement.viewAdapter;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.ldnr.punissement.R;
+import com.ldnr.punissement.model.Task;
+import com.ldnr.punissement.viewHolder.TaskViewHolder;
+
+import java.util.List;
+
+
+public class TaskAdapter extends RecyclerView.Adapter<TaskViewHolder> {
+
+    List<Task> list;
+
+    // constructor with an object list
+    public TaskAdapter(List<Task> list){
+        this.list = list;
+    }
+
+
+    // to create viewHolder
+    @Override
+    public TaskViewHolder onCreateViewHolder(ViewGroup viewGroup, int itemType){
+
+        View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.activity_tasklist, viewGroup, false);
+        return new TaskViewHolder(view);
+    }
+
+    // to fill the cell
+    @Override
+    public void onBindViewHolder(TaskViewHolder taskVH, int position){
+
+        Task task = list.get(position);
+        taskVH.blind(task);
+    }
+
+    @Override
+    public int getItemCount(){
+        return list.size();
+    }
+}
